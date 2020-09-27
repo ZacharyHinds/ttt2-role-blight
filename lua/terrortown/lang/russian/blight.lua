2264b76713da2582557caa3c1500fd9b700b687a
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 22:59:50 +0500

    Create blight.lua

---
+++ lua/terrortown/lang/russian/blight.lua
@@ -0,0 +1,13 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[BLIGHT.name] = "Скверна"
+L["info_popup_" .. BLIGHT.name] = [[Вы скверной! Заразите того, кто тебя убьет!]]
+L["body_found_" .. BLIGHT.abbr] = "Он был скверной!"
+L["search_role_" .. BLIGHT.abbr] = "Этот человек был скверной!"
+L["target_" .. BLIGHT.name] = "Скверна"
+L["ttt2_desc_" .. BLIGHT.name] = [[Скверна - предатель, который заражает человека, который его убивает!]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_blt_sick"] = "Вы чувствуете себя плохо"
+L["ttt2_blt_cured"] = "Вы чувствуете себя намного лучше"
